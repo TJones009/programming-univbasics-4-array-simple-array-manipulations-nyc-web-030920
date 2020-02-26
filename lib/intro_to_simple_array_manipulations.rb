e3e7270c2ae8baa939e3997c92c8c3670ade02c1
Trevor Jones <tjones009@yahoo.com>
2020-02-26 17:52:39 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -27,7 +27,7 @@ def using_concat(a, b)
 end
 
 def using_insert(array, x)
-  array.insert[4, x]
+  array.insert(4, x)
 end
 
 def using_uniq(array)
@@ -39,7 +39,7 @@ def using_delete(array, string)
 end
 
 def using_delete_at(array, integer)
-  array.delete_at[integer]
+  array.delete_at(integer)
 end
   
   
